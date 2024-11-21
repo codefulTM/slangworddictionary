c082a4f12a8fc16497bf9a0acd5c6ba9805856d3
codefulTM <nguyentuanminh435@gmail.com>
2024-11-21 13:30:10 +0700

    Created the Word class and its functions

---
+++ sources/Word.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project.sources;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Word {
+    private String name;
+    private String definition;
+    
+    public Word(String name, String definition) {
+        this.name = name;
+        this.definition = definition;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public String getDefinition() {
+        return this.definition;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+}
